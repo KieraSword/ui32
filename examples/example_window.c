2ece32cb6fb4a18c0e4a3c903295ea47c1b87d7d
KieraSword <kieranswordwastaken@gmail.com>
2025-01-18 11:55:40 +0000

    Update example_window.c
    Added SetVisible example along with fixing indents and comments

--- examples/example_window.c
+++ examples/example_window.c
@@ -14,9 +14,10 @@ int main(){
 	widget label = Label(window, "Hello, Win32!", 20, 30, 50, 50);	// Label() Parameters: Parent, Label, x, y, width, height
 	widget button = Button(window, "Click Me", 0, 20, 70, 60, 25); // Button() Parameters: Parent, Label, ID, x, y, width, height
 	bitmap image_gasmask = Image(NULL); // "C:\\Users\\Kiera\\Pictures\\en_gasmask.bmp"); // Image() Parameters: Path
-	widget imagebox = ImageBox(window, image_gasmask, 20, 120, 50, 50); // Button() Parameters: Parent, Image, x, y, width, height
-  widget edit = TextField(window, "", 190, 20, 120, 120); // TextField() Parameters: Parent, Preset Text, x, y, width, height
+	widget imagebox = ImageBox(window, image_gasmask, 20, 120, 50, 50); // ImageBox() Parameters: Parent, Image, x, y, width, height
+  	widget edit = TextField(window, "", 190, 20, 120, 120); // TextField() Parameters: Parent, Preset Text, x, y, width, height
 	font tahoma = Font("Tahoma", 18, 0, 0); // Font() Parameters: Font Name, Height, Bold (Bool), Italics (Bool)
-	SetFont(label, tahoma); // SetFont Parameters(): Widget, Font
+	SetFont(label, tahoma); // SetFont() Parameters: Widget, Font
+	SetVisible(window, true); // SetVisible() Parameters: Widget, Visibility (Bool)
 	WindowLoop(window); // WindowLoop Parameters(): Window
 }
